8af1934b82cdf9c03430c614930f64c1985e9046
Kelly Chhor <kcc343@gmail.com>
2018-05-01 19:55:28 -0700

    finish mapping with ggplot2

--- exercise-3/exercise.R
+++ exercise-3/exercise.R
@@ -7,13 +7,16 @@ library(dplyr)
 # Also install and load the `maps` package, which contains geometry for a number
 # of pre-defined maps.
 
+library(maps)
 
 # Use `map_data()` to load the `county` map of the US, storing it in a variable 
 # `counties_map`
 
+counties_map <- map_data("county")
 
 # Inspect this data frame to understand what data yu're working with
 
+View(counties_map)
 
 # Consider: what column contains state names? What column contains county names? 
 # What format are those county names in?
@@ -22,16 +25,21 @@ library(dplyr)
 # Map the the `x` aesthetic to longitude, `y` to latitude, and `group` to "group"
 # You can add the `coord_quick_map()` coordinate system to make it look nice
 
-
+ggplot(data = counties_map) +
+  geom_polygon(mapping = aes(x = long, y = lat, group = group)) +
+  coord_quickmap()
 
 ### Data Wrangling
 
 # Read in the provided election data file (.csv)
 # BE SURE TO SET YOUR WORKING DIRECTORY!
 
+election_data <- read.csv(
+  "data/2016_US_County_Level_Presidential_Results.csv", 
+  stringsAsFactors = FALSE
+  )
 
 # Inspect the `election` data frame to understand the data you're working with
-
 
 # Consider: what column contains state names? What column contains county names? 
 # What format are those county names in?
@@ -44,9 +52,11 @@ library(dplyr)
 
 # Use `data()` to load the `"county.fips"` data frame from the `maps` library
 
+data("county.fips")
 
 # Inspect the `county.fips` data frame to see what you got
 
+View(county.fips)
 
 # Use a `join` operation to add `fips` column from `county.fips` to your 
 # `counties_map` data frame.
@@ -54,17 +64,29 @@ library(dplyr)
 # "state,county" values to join by!
 # Also note: don't worry about Alaska for this exercise.
 
+counties_map <- counties_map %>% 
+  mutate(polyname = paste0(region, ",", subregion)) %>% 
+  left_join(county.fips, by = "polyname")
 
 # Now you can join the `counties_map` data (with fips!) to the `election` data
 # Hint: use `by = c("fips" = "combined_fips")` to specify the column to join by
 
+county_elections <- left_join(
+  counties_map, 
+  election_data, 
+  by = c("fips" = "combined_fips")
+  )
 
 # One more change: add a column to store whether the Democrat or the Republication
 # party had the higher number of votes ("won" the county)
 # Hint: start by adding a column of logical values (TRUE/FALSE) of whether a party
 #       won, and then join that with a simple data frame that matches to Strings
 
-
+county_elections <- county_elections %>% 
+  mutate(winner = votes_dem > votes_gop)
+dem_won <- data.frame(winner = c(T, F), party = c("Democrat", "Republican"))
+county_elections <- county_elections %>% 
+  left_join(dem_won, by = "winner")
 
 ### Data Visualization
 
@@ -74,5 +96,9 @@ library(dplyr)
 # Specify a `manual` fill scale to make Democratic counties "blue" and Republican
 # counties "red"
 
+ggplot(data = county_elections) +
+  geom_polygon(mapping = aes(x = long, y = lat, group = group, fill = party)) +
+  scale_fill_manual(values = c("blue", "red")) +
+  coord_quickmap()
 
 # For fun: how else can you fill in this map? What other insights can you produce?
